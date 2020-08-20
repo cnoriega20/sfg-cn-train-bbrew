2d6970cd713ceb8fcf43eb6e06fe0cf9cb9032b3
cnoriega20 <c79.noriega@gmail.com>
2020-08-20 15:52:32 -0500

    Added delete Endpoint and logging for BeerController

--- src/main/java/msvc/training/sfgcntrainbbrew/service/BeerService.java
+++ src/main/java/msvc/training/sfgcntrainbbrew/service/BeerService.java
@@ -10,4 +10,6 @@ public interface BeerService {
     Beer saveNewBeer(Beer beer);
 
     void updateBeer(UUID beerId, Beer beer);
+
+    void deleteById(UUID beerId);
 }
--- src/main/java/msvc/training/sfgcntrainbbrew/service/BeerServiceImpl.java
+++ src/main/java/msvc/training/sfgcntrainbbrew/service/BeerServiceImpl.java
@@ -1,14 +1,17 @@
 package msvc.training.sfgcntrainbbrew.service;
 
+import lombok.extern.slf4j.Slf4j;
 import msvc.training.sfgcntrainbbrew.model.Beer;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID beerId) {
+        log.debug("Get a beer...");
         return Beer.builder().id(UUID.randomUUID())
                 .beerName("Coors")
                 .beerStyle("Pilsener")
@@ -17,6 +20,7 @@ public class BeerServiceImpl implements BeerService {
 
     @Override
     public Beer saveNewBeer(Beer beer) {
+        log.debug("Saving a beer...");
         return Beer.builder()
                 .id(UUID.randomUUID())
                 .build();
@@ -25,5 +29,11 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, Beer beer) {
         //TODO: Add a real Impl with persistent data
+        log.debug("Updating a beer...");
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
