420a94918fab621fc2ceec38816d0b435a49364d
cnoriega20 <c79.noriega@gmail.com>
2020-08-20 09:09:59 -0500

    Project structure and gitignore

---
+++ src/main/java/msvc/training/sfgcntrainbbrew/SfgCnTrainBbrewApplication.java
@@ -0,0 +1,13 @@
+package msvc.training.sfgcntrainbbrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgCnTrainBbrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgCnTrainBbrewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/msvc/training/sfgcntrainbbrew/SfgCnTrainBbrewApplicationTests.java
@@ -0,0 +1,13 @@
+package msvc.training.sfgcntrainbbrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgCnTrainBbrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
