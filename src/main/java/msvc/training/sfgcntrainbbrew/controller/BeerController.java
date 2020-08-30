7ace87d081f0389034310adfd8eb1b93514f84d0
cnoriega20 <c79.noriega@gmail.com>
2020-08-30 16:16:50 -0500

    Request Mapping URI modified to fix wrong URL in client

--- src/main/java/msvc/training/sfgcntrainbbrew/controller/BeerController.java
+++ src/main/java/msvc/training/sfgcntrainbbrew/controller/BeerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/beerService")
+@RequestMapping("/api/v1/beerService/")
 public class BeerController {
 
     private final BeerService beerService;
